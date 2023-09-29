0011e24a44ad3cfdcdf179896f7b0b50eccc10e2
adeumal1 <deumaltripianaaleix@gmail.com>
2023-09-29 12:23:17 +0200

    creating new folders

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import './App.css'
 
-import Figure from './components/Figure'
+import Figure from './components/Figure/Figure'
 
 
 function App() {
