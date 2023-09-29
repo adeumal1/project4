e707d3728e60ab46bdf816cca4b528beae8a0e81
adeumal1 <deumaltripianaaleix@gmail.com>
2023-09-29 10:02:47 +0200

    pick date

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
 
 function App() {
-
+  // Recuperamos la fecha actual en un formato ISO -> 2023-01-01
+  const today = new Date(Date.now()).toISOString().slice(0, 10);
+  console.log(today);
   return (
     <>
       
