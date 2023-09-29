10474bbf8452982cafacb67a1cd53a65c0e1aa9c
adeumal1 <deumaltripianaaleix@gmail.com>
2023-09-29 10:12:17 +0200

    add input onchange date

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  // Recuperamos la fecha actual en un formato ISO -> 2023-01-01
+  
   const today = new Date(Date.now()).toISOString().slice(0, 10);
-  console.log(today);
+
+  const [date,setDate] = useState(today);
+
+  const handleInput = (e) => {
+    setDate(e.target.value.toLocaleString()); 
+  };
+  
   return (
     <>
-      
+      <input type='date' name='date' value={date} onChange={handleInput}></input>
     </>
   )
 }
