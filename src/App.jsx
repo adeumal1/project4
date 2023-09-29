ddd3f7c81c619cb6f003faa648fdb87377013908
adeumal1 <deumaltripianaaleix@gmail.com>
2023-09-29 14:00:42 +0200

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
 import './App.css'
 
-import Figure from './components/Figure/Figure'
-
+import Apod from './components/Apod/Apod'
+// import Mars from './components/Mars/Mars'
 
 function App() {
-  const today = new Date(Date.now()).toISOString().slice(0, 10);
-  const [date,setDate] = useState(today);
-
-  const [list, setList] = useState([]);
-
-  useEffect(()=>{
-    const NASA_URL = "https://api.nasa.gov/";
-    const NASA_API_KEY = "81GWdam7N8Wd1gEIso59h4iCZ2DMnCqpKvUZhk7V";
-
-    const getResponse = async () => {
-      try {
-          const response = await axios.get(`${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`);
-          setList(response.data);
-      } catch(err) {
-          console.log('err')
-      }
-    }
-    getResponse();
-  }, [date])
-  
-  const handleInput = (e) => {
-    setDate(e.target.value.toLocaleString()); 
-  };
   
   return (
-    <div className='main'>
-      <h1>Imagen astronómica del día</h1>
-      <input type='date' name='date' value={date} max={today} onChange={handleInput}></input>
-      <p>Esta imagen corresponde con la fecha {date}</p>
-      <Figure APOD={list}/>
-    </div>
+    <>
+        <Apod />
+        <br />
+        {/* <Mars /> */}
+
+    </>
   )
 }
 
---
+++ src/components/Apod/Apod.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+import Figure from '../Figure/Figure'
+
+const NASA_URL = "https://api.nasa.gov/";
+const NASA_API_KEY = "81GWdam7N8Wd1gEIso59h4iCZ2DMnCqpKvUZhk7V";
+
+const Apod = () => {
+  const today = new Date(Date.now()).toISOString().slice(0, 10);
+  const [date,setDate] = useState(today);
+
+  const [list, setList] = useState([]);
+
+  useEffect(()=>{
+
+    const getResponse = async () => {
+      try {
+          const response = await axios.get(`${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`);
+          setList(response.data);
+      } catch(err) {
+          console.log('err')
+      }
+    }
+    getResponse();
+  }, [date])
+  
+  const handleInput = (e) => {
+    setDate(e.target.value.toLocaleString()); 
+  };
+  
+  return (
+    <div className='main'>
+      <h1>Imagen astronómica del día</h1>
+      <input type='date' name='date' value={date} max={today} onChange={handleInput}></input>
+      <p>Esta imagen corresponde con la fecha {date}</p>
+      <Figure APOD={list}/>
+    </div>
+  )
+}
+
+export default Apod
