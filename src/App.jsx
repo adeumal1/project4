3e6800c9a3f5703fe40db48af22768f303152e5c
adeumal1 <deumaltripianaaleix@gmail.com>
2023-09-29 12:16:01 +0200

    adding responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import './App.css'
-import Figure from './components/Figure'
 import axios from 'axios';
+import './App.css'
+
+import Figure from './components/Figure'
+
 
 function App() {
-  const NASA_URL = "https://api.nasa.gov/";
-  const NASA_API_KEY = "81GWdam7N8Wd1gEIso59h4iCZ2DMnCqpKvUZhk7V";
-  
   const today = new Date(Date.now()).toISOString().slice(0, 10);
   const [date,setDate] = useState(today);
 
   const [list, setList] = useState([]);
 
   useEffect(()=>{
+    const NASA_URL = "https://api.nasa.gov/";
+    const NASA_API_KEY = "81GWdam7N8Wd1gEIso59h4iCZ2DMnCqpKvUZhk7V";
+
     const getResponse = async () => {
       try {
-           const response = await axios.get(`${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`);
-           setList(response.data);
+          const response = await axios.get(`${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`);
+          setList(response.data);
       } catch(err) {
-           console.log('err')
+          console.log('err')
       }
     }
     getResponse();
@@ -29,12 +31,12 @@ function App() {
   };
   
   return (
-    <>
+    <div className='main'>
       <h1>Imagen astronómica del día</h1>
       <input type='date' name='date' value={date} max={today} onChange={handleInput}></input>
       <p>Esta imagen corresponde con la fecha {date}</p>
       <Figure APOD={list}/>
-    </>
+    </div>
   )
 }
 
--- src/components/Figure.jsx
+++ src/components/Figure.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Figure.css'
 
 const Figure = ({ APOD }) => {
   return (
